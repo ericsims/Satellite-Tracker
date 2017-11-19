b927ef8269fd0f6059d08243b6e718ab218ea515
Giselle Koo <gisellegk@gmail.com>
2017-11-18 22:30:11 -0500

    this is fine *house burning down*

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -12,10 +12,14 @@ Modified 18 Nov 2017 by Giselle Koo
 
 Serial pc(SERIAL_TX, SERIAL_RX);
 
-#define ALT1 p13
-#define ALT2 p14
-#define ALT3 p15
-#define ALT4 p16
+// pin assignments
+
+#define ALT1 p12
+#define ALT2 p13
+#define ALT3 p14
+#define ALT4 p15
+
+#define ALTHOME p16
 
 #define AZ1 p17
 #define AZ2 p18
@@ -28,22 +32,19 @@ Serial pc(SERIAL_TX, SERIAL_RX);
 // TLE lines 1 and 2
 char longstr1[];
 char longstr2[];
-
 // Site location data
 double siteLat, siteLon, siteAlt, siteLatRad, siteLonRad;
 
 //Function prototypes
-float Convert_El_to_Servo(float elevation);
 void setTime(int time);
 void setTLE(char[] TLE1, char[] TLE2);
 void setLocData(double lat, double lon, double alt)
 
 void setTime(int time) {
-  // time in epoch
-  set_time(time);
+  set_time(time); // time in epoch
 }
 
-void setTLE(char[] TLE1, char[] TLE2){
+void setTLE(char[] TLE1, char[] TLE2) {
   longstr1 = TLE1;
   longstr2 = TLE2;
 }
@@ -56,13 +57,16 @@ void setLocData(double lat, double lon, double alt) {
   siteLonRad = siteLon * pi / 180.0;
 }
 
+
 int main()
 {
     //INITIALIZE AZIMUTH STEPPER MOTOR
     Stepper *azMotor = Stepper(AZ1, AZ2, AZ3, AZ4, AZHOME);
-    azMotor->findHome();
+    azMotor->findHome(); // home should be pointing North
 
     //INITIALIZE ALTITUDE STEPPER MOTOR
+    Stepper *altMotor = Stepper(ALT1, ALT2, ALT3, ALT4, ALTHOME);
+    altMotor->findHome(); // home should be pointing -90 deg (straight down)
 
     //SET UP SOME VARIABLES
     double ro[3];
@@ -82,22 +86,9 @@ int main()
     typedef char str3[4];
     str3 monstr[13];
     elsetrec satrec;
-    double steps_per_degree = azMotor->getStepsPerDegree(); //Stepper motor steps per degree azimuth
-    float elevation;
 
-    //VARIABLES FOR STEPPER CALCULATIONS
+    float elevation;
     float azimuth; //-180 to 0 to 180
-    float prevAzimuth;
-    float cAzimuth; //From 0 to 359.99
-    float prevcAzimuth;
-    bool stepperRelative = 0; //Has the stepper direction been initialized?
-    float azimuthDatum = 0;
-    int stepsFromDatum = 0;
-    int stepsNext = 0;
-    int dirNext = 1;
-    int totalSteps = 0;
-    int prevDir = 3; //Initialize at 3 to indicate that there is no previous direction yet (you can't have a "previous direction" until the third step)
-    double azError = 0;
 
     //SET REAL TIME CLOCK (Set values manually using custom excel function until I find a way to do it automatically)
     set_time(1440763200);
@@ -122,33 +113,15 @@ int main()
     strcpy(monstr[12], "Dec");
 
     //ENTER TWO-LINE ELEMENT HERE
-    longstr1 = "1 25544U 98067A   15239.40934558  .00012538  00000-0  18683-3 0  9996";
-    longstr2 = "2 25544  51.6452  88.4122 0001595  95.9665 324.8493 15.55497522959124";
+    longstr1 = "1 25544U 98067A   17322.95053927  .00004162  00000-0  69955-4 0  9996";
+    longstr2 = "2 25544  51.6415 346.8032 0004306 128.6427 307.0316 15.54181392 85825";
 
     //ENTER SITE DETAILS HERE
-    siteLat = 30.25; //+North (Austin)
-    siteLon = -97.75; //+East (Austin)
-    siteAlt = 0.15; //km (Austin)
+    siteLat = 41.798599; //+North (UHart)
+    siteLon = -72.714000; //+East (UHart)
+    siteAlt = 0.0143; //km (UHart)
     siteLatRad = siteLat * pi / 180.0;
     siteLonRad = siteLon * pi / 180.0;
-
-    //FREEDOM OF MOVEMENT CHECKS
-
-    for (int i = 0; i < 500; i = i + 10) {
-        EL_SERVO = Convert_El_to_Servo(-90.0 + 180.0 * i / 500.0);
-    }
-    wait(1);
-
-    for (int i = 500; i > 0; i = i - 10) {
-        EL_SERVO = Convert_El_to_Servo(-90.0 + 180.0 * i / 500.0);
-    }
-    wait(1);
-
-    /*
-    //FREEDOM OF MOVEMENT CHECKS STEPPER
-    azMotor->step(500, FORWARD, SINGLE);
-    azMotor->step(500, BACKWARD, SINGLE);
-    */
 
     //INITIALIZE SATELLITE TRACKING
     //pc.printf("Initializing satellite orbit...\n");
@@ -170,7 +143,6 @@ int main()
     //BEGIN SATELLITE TRACKING
     while(1)
     {
-
         //RUN SGP4 AND COORDINATE TRANSFORMATION COMPUTATIONS
         jdC = getJulianFromUnix(time(NULL));
         tsince = (jdC - jd) * 24.0 * 60.0;
@@ -187,127 +159,21 @@ int main()
         else
         {
             azimuth = razel[1]*180/pi;
-            if (azimuth < 0) {
-                cAzimuth = 360.0 + azimuth;
-            }
-            else {
-                cAzimuth = azimuth;
-            }
             elevation = razel[2]*180/pi;
+
+            azMotor->setAngle((double)azimuth);
+            altMotor->setAngle((double)elevation);
+
+            azMotor->stepperLoop();
+            altMotor->stepperLoop();
 
             //pc.printf("%16.8f%16.8f%16.8f%16.8f%16.8f%16.8f%16.8f\n", satrec.t, recef[0], recef[1], recef[2], vecef[0], vecef[1], vecef[2]);
             //pc.printf("%16.8f%16.8f%16.8f%16.8f%16.8f%16.8f%16.8f\n", satrec.t, latlongh[0]*180/pi, latlongh[1]*180/pi, latlongh[2], razel[0], razel[1]*180/pi, razel[2]*180/pi);
 
-            //For first step, initialize the stepper direction assuming its initial position is true north
-            if (stepperRelative == 0){
-                stepsNext = int(cAzimuth * steps_per_degree);
-                dirNext = 2;
-                azMotor->step(stepsNext, dirNext, MICROSTEP); //Turn stepper clockwise to approximate initial azimuth
-                stepperRelative = 1;
-                azimuthDatum = stepsNext / steps_per_degree;
-                prevAzimuth = azimuth;
-                prevcAzimuth = cAzimuth;
-
-                pc.printf("             Azimuth       Azimuth Datum    Steps from Datum         Total Steps          Steps Next           Direction          Az. Error\n");
-            }
-            else {
-
-                //Determine direction of rotation (note this will be incorrect if azimuth has crossed true north since previous step - this is dealt with later)
-                if ( cAzimuth < prevcAzimuth ) {
-                    dirNext = 1; //CCW
-                }
-                else {
-                    dirNext = 2; //CW
-                }
-
-
-                //Check if azimuth has crossed from 360 to 0 degrees or vice versa
-                if (abs( (azimuth - prevAzimuth) - (cAzimuth - prevcAzimuth) ) > 0.0001) {
-
-                    //Recalculate direction of rotation
-                    if ( cAzimuth > prevcAzimuth ) {
-                        dirNext = 1; //CCW
-                    }
-                    else {
-                        dirNext = 2; //CW
-                    }
-
-                    //Reset the azimuth datum
-                    if (dirNext == 1) {
-                        azimuthDatum = cAzimuth + azError + prevcAzimuth;
-                    }
-                    else {
-                        azimuthDatum = cAzimuth - azError + (prevcAzimuth - 360);
-                    }
-
-                    //Reset totalSteps
-                    totalSteps = 0;
-                }
-
-
-                //Check if azimuth rate has changed directions
-                if (prevDir != 3) { //prevDir of 3 means there is no previous direction yet
-
-                    if (prevDir != dirNext) {
-
-                        //Reset totalSteps
-                        totalSteps = 0;
-
-                        //Reset azimuth datum
-                        if (dirNext == 1) {
-                            azimuthDatum = prevcAzimuth + azError;
-                        }
-                        else {
-                            azimuthDatum = prevcAzimuth - azError;
-                        }
-
-                    }
-
-                }
-
-
-                stepsFromDatum = int( abs(cAzimuth - azimuthDatum) * steps_per_degree );
-                stepsNext = stepsFromDatum - totalSteps;
-                totalSteps += stepsNext;
-                azError = abs(cAzimuth - azimuthDatum) - (totalSteps / steps_per_degree);
-
-                pc.printf("%20.2f%20.2f%20d%20d%20d%20d%20.2f\n", cAzimuth, azimuthDatum, stepsFromDatum, totalSteps, stepsNext, dirNext, azError);
-
-                if (stepsNext > 250) {
-
-                    pc.printf("something's probably wrong... too many steps\n\n\n\n");
-                    while(1){} // pause
-
-                }
-
-                azMotor->step(stepsNext, dirNext, MICROSTEP);
-            }
-
-            EL_SERVO = Convert_El_to_Servo(elevation);
-            prevAzimuth = azimuth;
-            prevcAzimuth = cAzimuth;
-            prevDir = dirNext;
         }
 
         wait(1);
 
-    } //indefinite loop
-
-}
-
-float Convert_El_to_Servo(float elevation) {
-
-    float servo_90     = 0.71;     //Calibrated to servo
-    float servo_0      = 0.445;    //Calibrated to servo
-    float servo_min    = 0.186;
-    float servo_ratio;
-
-    //Interpolate servo ratio
-    servo_ratio = servo_0 + (servo_90 - servo_0) * (elevation) / (90);
-    if (servo_ratio < servo_min) {
-        return servo_min;
     }
-    else {
-        return servo_ratio;
-    }
+
 }
